816f8c975a8be3ef989d4141172458a493fe47b8
N.C.R <64447096+kuyaxxx@users.noreply.github.com>
2022-11-02 12:33:23 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ if [ -f "/etc/xray/domain" ]; then
 echo "Script Already Installed"
 exit 0
 fi
-
+domain=xray.kuyax.tech
 apt install iptables iptables-persistent -y
 apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
 apt install socat cron bash-completion ntpdate -y
