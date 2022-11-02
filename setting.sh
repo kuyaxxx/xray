3c523f8be42e67f8e6ff9657706a6576ed9fa6ea
N.C.R <64447096+kuyaxxx@users.noreply.github.com>
2022-11-02 23:29:19 +0000

    Create setting.sh

---
+++ setting.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#!/bin/bash
+clear
+m="\033[0;1;36m"
+y="\033[0;1;37m"
+yy="\033[0;1;32m"
+yl="\033[0;1;33m"
+wh="\033[0m"
+echo -e "$y                         SETTING$wh"
+echo -e "$y-------------------------------------------------------------$wh"
+echo -e "$yy 1$y.  Add Or Change Subdomain Host For VPS"
+echo -e "$yy 2$y.  Change Port Of Some Service"
+echo -e "$yy 3$y.  Autobackup Data VPS"
+echo -e "$yy 4$y.  Backup Data VPS"
+echo -e "$yy 5$y.  Restore Data VPS"
+echo -e "$yy 6$y.  Limit Bandwith Speed Server"
+echo -e "$yy 7$y.  Check Usage of VPS Ram"
+echo -e "$yy 8$y.  Reboot VPS"
+echo -e "$yy 9$y.  Speedtest VPS"
+echo -e "$yy 10$y. Displaying System Information"
+echo -e "$yy *$y.  Menu"
+echo -e "$yy 99$y. Exit"
+echo -e "$y-------------------------------------------------------------$wh"
+echo -e ""
+read -p "Select From Options : " menu
+echo -e ""
+case $menu in
+1)
+addhost
+;;
+2)
+changeport
+;;
+3)
+autobackup
+;;
+4)
+backup
+;;
+5)
+restore
+;;
+6)
+limitspeed
+;;
+7)
+ram
+;;
+8)
+reboot
+;;
+9)
+speedtest
+;;
+10)
+info
+;;
+*)
+clear
+menu
+;;
+99)
+clear
+exit
+;;
+esac
