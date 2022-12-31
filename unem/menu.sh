c857587a97933051f603d5b977f515616315b914
N.C.R <64447096+kuyaxxx@users.noreply.github.com>
2022-12-31 09:53:53 +0700

    Update menu.sh

--- unem/menu.sh
+++ unem/menu.sh
@@ -2,14 +2,8 @@
 # ==========================================
 ncrvpn="raw.githubusercontent.com/kuyaxxx/xray/main"
 wget -O /usr/bin/menu "https://${ncrvpn}/unem/menu"
-wget -O /usr/bin/traffic "https://${ncrvpn}/unem/traffic"
-wget -O /usr/bin/menu-xray "https://${ncrvpn}/unem/menu-xray"
 wget -O /usr/bin/add-xray "https://${ncrvpn}/unem/menu-xray/add-xray"
-wget -O /usr/bin/menu-xray "https://${ncrvpn}/unem/menu-ssh"
 
 #
 chmod +x /usr/bin/menu
-chmod +x /usr/bin/traffic
-chmod +x /usr/bin/menu-xray
 chmod +x /usr/bin/add-xray
-chmod +x /usr/bin/menu-ssh
